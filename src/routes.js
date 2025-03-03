34c8504a74a29e4fe19accefe85f21c7b8a8c4a6
Diego__Leite <diegoleite59986@gmail.com>
2025-03-03 20:49:44 -0300

    api

--- src/routes.js
+++ src/routes.js
@@ -7,25 +7,29 @@ import multerConfig from './config/multer';
 import authMiddleware from "./app/middlewares/auth";
 import CategoryController from "./app/controllers/CategoryController ";
 import OrderController from "./app/controllers/OrderController";
-
+import CreatePaymentIntentController from "./app/controllers/stripe/CreatePaymentIntent"
 const routes = new Router()
 const upload = multer(multerConfig);
 
-routes.post('/users', UserControler.store);
-routes.post('/session' , SessionController.store)
+routes.post('/users', UserControler.store); //Cadastro
 
- routes.use(authMiddleware);
+routes.post('/session' , SessionController.store) // Login 
+
+ routes.use(authMiddleware); // será chamado por todas as rotas Abaixo f
+
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products',  ProductController.index);// faz um opload de apenas um arquivo , chama o nosso controler ]
 routes.put('/products/:id', upload.single('file'), ProductController.update)
 
 routes.post('/categories', upload.single('file'), CategoryController.store)
-routes.get('/categories',  ProductController.index);
+routes.get('/categories',  CategoryController.index);
 routes.put('/categories/:id', upload.single('file'), CategoryController.update)
 
-routes.post('/orders', OrderController.store)
-routes.get('/orders', OrderController.index)
-routes.put('/orders/:id', OrderController.update)
+routes.post('/orders', OrderController.store) //CRIAR UM NOVO PEDIDO
+routes.get('/orders', OrderController.index) // ACESSAR OS PEDIDOS
+routes.put('/orders/:id', OrderController.update) // ALTERAÇÃO NOS PEDIDOS
+
+routes.post("/create-payment-intent", CreatePaymentIntentController.store);
 
 export default routes
 
